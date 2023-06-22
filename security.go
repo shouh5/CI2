16bcc05c0081e7f6cf0944fb43aa6d5448f8713e
shouh5 <abcd60@qq.com>
2023-06-22 15:54:17 +0800

    安全检查-密码问题

--- security.go
+++ security.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func security(){
-	var password = "123456"
+	//var password = "123456"
 
-	//var password = os.Getenv("password")
+	var password = os.Getenv("password")
 
 	fmt.Printf("my password is %s", password)
 }
